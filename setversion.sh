9db5617448ba8d85ecfb6277da3fc3698c46bbc7
Marco Wehnert <marco.wehnert@gmx.de>
2020-02-17 12:46:51 +0100

    setversion.sh changes now also the version number in script start.sh

--- setversion.sh
+++ setversion.sh
@@ -11,3 +11,5 @@ fi
 # Modify build.gradle
 sed -i s/'version.*=.*'/'version = '\'"$version"\'/g build.gradle
 
+sed -i s/'doip-custom-simulation-.*'.'.*'.'.*'.jar/'doip-custom-simulation-'$version'.jar'/g src/main/dist/start.sh
+
--- src/main/dist/start.sh
+++ src/main/dist/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dlog4jConfigurationFile="log4j2.xml" -jar libs/doip-custom-simulation-1.1.2.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -Dlog4jConfigurationFile="log4j2.xml" -jar libs/doip-custom-simulation-1.1.3.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
