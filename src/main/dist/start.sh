775700e8fd19348d66a5ff76f0ab4dbafbecb402
Marco Wehnert <marco.wehnert@gmx.de>
2023-06-17 04:11:19 +0100

    Change arguments of application, now there is the option -i available for interactive mode; Multiple gateways can be started now, just pass multiple arguments with property files.

--- src/main/dist/start.sh
+++ src/main/dist/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dlog4jConfigurationFile="log4j2.xml" -jar libs/doip-custom-simulation-1.2.0.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -Dlog4jConfigurationFile="log4j2.xml" -jar libs/doip-custom-simulation-1.2.0.jar "$@"
