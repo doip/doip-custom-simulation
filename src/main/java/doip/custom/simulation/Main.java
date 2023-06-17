4adad7e648c80ad2545502455502993a1a07cb2b
Marco Wehnert <marco.wehnert@gmx.de>
2023-06-17 04:11:19 +0100

    Change arguments of application, now there is the option -i available for interactive mode; Multiple gateways can be started now, just pass multiple arguments with property files.

--- src/main/java/doip/custom/simulation/Main.java
+++ src/main/java/doip/custom/simulation/Main.java
@@ -3,6 +3,7 @@ package doip.custom.simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 import doip.library.properties.EmptyPropertyValue;
 import doip.library.properties.MissingProperty;
@@ -11,29 +12,61 @@ import doip.simulation.nodes.GatewayConfig;
 public class Main {
 
 	public static void main(String[] args) throws IOException, MissingProperty, EmptyPropertyValue {
-		if (args.length != 1) {
-			System.out.println("Invalid number of arguments.");
-			System.out.println("Usage:");
-			System.out.println("    ./start.sh <property-file>");
-			System.out.println("Example:");
-			System.out.println("    ./start.sh gateway.properties");
+		
+		boolean interactive = false;
+		LinkedList<String> propertyFiles = new LinkedList<String>();
+		
+		for (int i = 0; i < args.length; i++) {
+			if ("-i".equals(args[i]) || "--interactive".equals(args[i])) {
+				interactive = true;
+			} else {
+				propertyFiles.add(args[i]);
+			}
+		}
+		
+		if (propertyFiles.size() == 0) {
+			System.out.println("No argument for property file was given.");
 			System.exit(1);
 		}
 
+
+		int numberOfGateways = propertyFiles.size();
+		GatewayConfig configs[] = new GatewayConfig[numberOfGateways];
+		CustomGateway gateways[] = new CustomGateway[numberOfGateways];
+		
+		int i = 0;
+		for (String propertyFile : propertyFiles) {
+			configs[i] = new GatewayConfig();
+			System.out.println("Load gateway configuration \"" + propertyFile + "\"");
+			configs[i].loadFromFile(propertyFile);
+			i++;
+		}
+		
+		for (i = 0; i<numberOfGateways; i++) {
+			gateways[i] = new CustomGateway(configs[i]);
+		}
+		
 		System.out.println("Starting DoIP simulation ...");
+		for (i = 0; i<numberOfGateways; i++) {
+			gateways[i].start();
+		}
 
-		GatewayConfig config = new GatewayConfig();
+		if (interactive) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Press <Enter> to terminate simulation");
+			reader.readLine();
+		} else {
+			for(;;) {
+				try {
+					Thread.sleep(60000);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
 
-		config.loadFromFile(args[0]);
-
-		CustomGateway gateway = new CustomGateway(config);
-		gateway.start();
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Press <Enter> to terminate simulation");
-		reader.readLine();
-
-		gateway.stop();
+		for (i = 0; i < numberOfGateways; i++) {
+			gateways[i].stop();
+		}
 		System.out.println("... DoIP simulation finished.");
 	}
 }
